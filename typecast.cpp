0555e9ee25eb97fb42eafd1c7ab09d25f1a159a2
Naavarasi N R <84852191+nivimani171016@users.noreply.github.com>
2021-06-11 22:39:30 +0530

    Add files via upload

---
+++ typecast.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+int a,b,c,d;
+float e,f,g;
+cout <<" Enter the value of a";
+cin >>a;
+cout <<"\n Enter the value of b";
+cin >>b;
+cout <<" \n Enter the value of c";
+cin >>c;
+cout <<"\n Enter the value of d";
+cin >>d;
+cout <<"\n";
+
+e=((float)a/(float)b); 
+cout <<"\n The value of e is"<<e<<"\n";
+
+f=((float)c/(float)d);
+cout <<"\n The value of f now is"<<f<<"\n";
+
+g=e+f;
+cout <<"\n The value of gg now is" <<g <<"\n";
+
+
+return 0;
+}
