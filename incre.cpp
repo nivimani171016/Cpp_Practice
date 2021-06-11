97a0264e65940c8352b8b2f7ce8a8119efafa575
Naavarasi N R <84852191+nivimani171016@users.noreply.github.com>
2021-06-11 22:34:32 +0530

    Add files via upload

--- incre.cpp
+++ incre.cpp
@@ -4,25 +4,25 @@ using namespace std;
 int main()
 {
 int a=1;
-cout <<"The value of a is"<<a++<<"\n";
+cout <<"The value of a is"<<a++<<"\n"; //post increment 
 cout <<"The value of a now is"<<a<<"\n";
 cout <<"\n";
 
 a=1;
 cout <<"The value of a before increment is"<<a<<"\n";
-cout <<"The value of a is"<<++a<<"\n";
+cout <<"The value of a is"<<++a<<"\n";//pre increment 
 cout <<"The value of a now is"<<a<<"\n";
 cout <<"\n";
 
 a=1;
 cout <<"The value of a before decrement is"<<a<<"\n";
-cout <<"The value of a is"<<a--<<"\n";
+cout <<"The value of a is"<<a--<<"\n"; //post decrement 
 cout <<"The value of a now is"<<a<<"\n";
 cout <<"\n";
 
 a=1;
 cout <<"The value of a before decrement is"<<a<<"\n";
-cout <<"The value of a is"<<--a<<"\n";
+cout <<"The value of a is"<<--a<<"\n"; //pre decrement 
 cout <<"The value of a now is"<<a<<"\n";
 cout <<"\n";
 
